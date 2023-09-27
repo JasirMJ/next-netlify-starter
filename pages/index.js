36821b8761215ac87f25ba5f7c302f1f57aa3889
Mohamed Jasir Kp <jasirmj@gmail.com>
2023-09-27 12:28:01 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next.js Starter! Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
